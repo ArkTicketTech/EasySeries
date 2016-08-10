bc869b75901fc21818f59753930e6be1e472fce1
wfnuser <wfnuser@126.com>
2016-08-10 18:15:16 +0800

    [FEATURE] ads show api response json

--- series/app/controllers/ads_controller.rb
+++ series/app/controllers/ads_controller.rb
@@ -4,9 +4,19 @@ class AdsController < ApplicationController
 
 	def create
 		@ad = Ad.new(ad_params)
-
 		@ad.save
 		redirect_to @ad
+	end
+
+	def show
+		
+	  @ad = Ad.find(params[:id])
+
+	  respond_to do |format|
+	  	format.html #show.html.erb
+	  	format.json {render :json => @ad}
+	  end
+
 	end
 
 	private
