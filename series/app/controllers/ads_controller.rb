2174177860e14ff2cc30f432f46aa9b7f60eb66e
wfnuser <wfnuser@126.com>
2016-08-10 19:38:15 +0800

    [FEATURE] finish the api of ads (CRUD)

--- series/app/controllers/ads_controller.rb
+++ series/app/controllers/ads_controller.rb
@@ -1,15 +1,26 @@
 class AdsController < ApplicationController
+
+	skip_before_filter :verify_authenticity_token
+
 	def new
 	end
 
 	def create
+
 		@ad = Ad.new(ad_params)
-		@ad.save
-		redirect_to @ad
+		
+		respond_to do |format|
+      if @ad.save
+        format.json { render :json => @ad }
+      else
+        format.json { render json: @ad.errors }
+      end
+    end
+
 	end
 
 	def show
-		
+
 	  @ad = Ad.find(params[:id])
 
 	  respond_to do |format|
@@ -19,7 +30,33 @@ class AdsController < ApplicationController
 
 	end
 
+	def update
+
+		@ad = Ad.find(params[:id])
+
+    respond_to do |format|
+      if @ad.update(ad_params)
+        format.json { head :no_content }
+      else
+        format.json { render json: @ad.errors }
+      end
+    end
+
+  end
+
+  def destroy
+
+  	@ad = Ad.find(params[:id])
+
+    @ad.destroy
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+
+  end
+
 	private
+
 		def ad_params
 			params.require(:ad).permit(:url, :img, :audit, :activate, :describe)
 		end
--- series/config/routes.rb
+++ series/config/routes.rb
@@ -55,5 +55,6 @@ Rails.application.routes.draw do
   #   end
 
   resources :ads
+  resources :series
 
 end
